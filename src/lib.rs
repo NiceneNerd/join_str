b4ce983f8a0afc84eb416afe522e70fdac29ed5c
Caleb Smith <caleb@myrvmail.com>
2021-10-08 12:50:55 -0500

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+use proc_macro::TokenStream;
+
+#[proc_macro]
+pub fn fstr(item: TokenStream) -> TokenStream {
+    let mut string = String::from("[");
+    string.push_str(
+        &item
+            .to_string()
+            .trim_start_matches("r#")
+            .trim_end_matches("#"),
+    );
+    string.push_str("].join(\"\")");
+    string
+        .replace("{", "\", ")
+        .replace("}", ", \"")
+        .replace("\"\", ", "")
+        .parse()
+        .unwrap()
+}
